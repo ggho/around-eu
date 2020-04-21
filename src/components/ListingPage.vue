a14b9db8cb686768710c0467e5f503ff70534f71
Gigi Ho <gigiho609@gmail.com>
2020-04-21 13:43:50 +0800

    UI for Route details

--- src/components/ListingPage.vue
+++ src/components/ListingPage.vue
@@ -2,38 +2,78 @@
   <v-container>
     <v-row class="mt-3" justify="center" no-gutters>
       <v-col cols="12">
-        <h1 class="display-1 font-weight-bold">Choose Outbound</h1>
+        <h1 class="display-1 font-weight-bold">Choose outbound</h1>
       </v-col>
     </v-row>
 
-    <v-row class="mt-6" no-gutters>Sorted by: Fast & Cheap</v-row>
-
-    <v-row class="mt-6" justify="center" no-gutters>
+    <v-row class="mt-3">
       <v-col cols="12">
-        <v-card v-for="(item, i) in routes" :key="i" class="mt-3">
-          <v-container>
-            <div v-text="item.most"></div>
+        <v-chip link>Vehicles</v-chip>
+        <v-chip link class="ml-1">Transfers</v-chip>
+        <v-chip link class="ml-1">Times</v-chip>
+        <v-chip link class="ml-1">Durations</v-chip>
+      </v-col>
+    </v-row>
 
-            <div>
-              <span v-text="item.timeFrom"></span>
-              <span>&nbsp;-&nbsp;</span>
-              <span v-text="item.timeTo"></span>
-            </div>
-            <div>
-              <span v-text="item.locationFrom"></span>
-              <span>&nbsp;-&nbsp;</span>
-              <span v-text="item.locationTo"></span>
-            </div>
-            <div class="d-flex justify-space-between">
-              <div>
-                <v-icon v-text="item.vehicle"></v-icon>
-                <span v-text="item.transfers"></span>
-                <span>&nbsp;Transfers</span>
+    <v-row class="mt-3" no-gutters>Sorted by: Fast & Cheap</v-row>
+
+    <v-row class="mt-3" justify="center" no-gutters>
+      <v-col cols="12">
+        <template v-for="(item, i) in routes">
+          <v-card class="mt-3" :key="i">
+            <v-container>
+              <div class="d-flex justify-space-between">
+                <div v-text="item.most" class="text-uppercase"></div>
+                <div>
+                  CO2
+                  <span v-text="item.co2"></span>
+                  kg
+                </div>
               </div>
-              <div v-text="item.price"></div>
-            </div>
-          </v-container>
-        </v-card>
+
+              <div>
+                <span v-text="item.timeFrom" class="font-weight-bold" style="font-size: 1.4em;"></span>
+                <span>&#32;—&#32;</span>
+                <span v-text="item.duration" style="font-size:0.8em"></span>
+                <span>&#32;—&#32;</span>
+                <span v-text="item.timeTo" class="font-weight-bold" style="font-size: 1.4em;"></span>
+                <span v-show="item.timeToPlus1" style="font-size:0.8em">+1</span>
+              </div>
+              <div>{{item.locationFrom}} - {{item.locationTo}}</div>
+              <div class="d-flex justify-space-between">
+                <div @click="item.showDetails = !item.showDetails">
+                  <v-icon>{{item.vehicle}}</v-icon>
+                  <span class="ml-1">{{item.transfers}}</span>
+                  <span class="ml-1">Transfer</span>
+                  <v-icon>{{item.showDetails ? 'keyboard_arrow_up' : 'keyboard_arrow_down'}}</v-icon>
+                </div>
+
+                <div v-text="item.price"></div>
+              </div>
+
+              <div v-show="item.showDetails">
+                <v-divider class="my-6"></v-divider>
+                <v-row v-for="(details, i) in item.routeDetails" :key="i" no-gutters>
+                  <v-col cols="3">
+                    <div class="font-weight-bold">{{details.time}}</div>
+                    <div>{{details.duration}}</div>
+                  </v-col>
+                  <v-col
+                    cols="9"
+                    class="pl-2"
+                    :style="'border-left: 2px ' + (i%2 == 0 ? 'solid' : 'dotted')"
+                  >
+                    <div
+                      :class="(i == 0 || i == item.routeDetails.length-1) ? 'font-weight-bold' : ''"
+                    >{{details.station}}</div>
+                    <div>{{details.carrier}}</div>
+                    <v-spacer v-show="i < (item.routeDetails.length-1)" style="height: 2em;"></v-spacer>
+                  </v-col>
+                </v-row>
+              </div>
+            </v-container>
+          </v-card>
+        </template>
       </v-col>
     </v-row>
   </v-container>
@@ -62,7 +102,47 @@ export default {
         locationTo: "Hamburg",
         transfers: 4,
         vehicle: "train",
-        price: "€120"
+        price: "€120",
+        co2: "27",
+        showDetails: false,
+        routeDetails: [
+          {
+            time: "12:21",
+            station: "Stokcholm Cityterminalen",
+            carrier: "Flixbus",
+            duration: "8h55"
+          },
+          {
+            time: "0:00",
+            timePlus1: true,
+            station: "København, Ingerslevsgade (Central Bus Station)",
+            transferDuration: "1h30"
+          },
+          {
+            time: "00:00",
+            station: "Stokcholm Cityterminalen",
+            carrier: "Flixbus",
+            duration: "8h55"
+          },
+          {
+            time: "6:05",
+            timePlus1: true,
+            station: "København, Ingerslevsgade (Central Bus Station)",
+            transferDuration: "1h30"
+          },
+          {
+            time: "7:35",
+            station: "København, Ingerslevsgade (Central Bus Station)",
+            carrier: "Flixbus",
+            duration: "5h40"
+          },
+          {
+            time: "13:15",
+            timePlus1: false,
+            station: "Hamburg, ZOB am Hbf",
+            transferDuration: ""
+          }
+        ]
       },
       {
         most: "Cheapest",
@@ -74,7 +154,35 @@ export default {
         locationTo: "Hamburg",
         transfers: 1,
         vehicle: "directions_bus",
-        price: "€111"
+        price: "€111",
+        co2: "25",
+        showDetails: false,
+        routeDetails: [
+          {
+            time: "21:10",
+            station: "Stokcholm Cityterminalen",
+            carrier: "Flixbus",
+            duration: "8h55"
+          },
+          {
+            time: "6:05",
+            timePlus1: true,
+            station: "København, Ingerslevsgade (Central Bus Station)",
+            transferDuration: "1h30"
+          },
+          {
+            time: "7:35",
+            station: "København, Ingerslevsgade (Central Bus Station)",
+            carrier: "Flixbus",
+            duration: "5h40"
+          },
+          {
+            time: "13:15",
+            timePlus1: false,
+            station: "Hamburg, ZOB am Hbf",
+            transferDuration: ""
+          }
+        ]
       }
     ]
   })
